dc668acdd84dce4fdc77545d3f74c5f365d93a70
SummerEcho <164468785+EchosOfSummer@users.noreply.github.com>
2025-09-09 20:33:53 -0500

    Assignment 3 done.

--- site.js
+++ site.js
@@ -1,1 +1,41 @@
-console.log("Site.js loaded successfully!");
+
+// orginally had function () , but changed it to arrow function after playing around for a while and realized i could change.
+document.addEventListener('DOMContentLoaded', () => {
+    const hours = new Date().getHours() // get the current hour
+
+    const isMorning = hours >= 4 && hours < 12 // is it morning?
+    const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+    const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+    let messages
+    if (isMorning) {
+        messages = ['Good morning! Drink Coffee!', 'WAKE UP!']
+    } else if (isAfternoon) {
+        messages = ['Good afternoon! Take a Break!', 'TIME TO WORK!']
+    } else if (isEvening) {
+        messages = ['Good evening! Time to Relax!', 'TIME TO RELAX!']
+    }
+
+    // originally hd getElementById('welcome') but was curious if i could select an element within and eventually found out i couuld. then figured out a shorter way to write it.
+    const welcome = document.querySelector('#welcome h3')
+    // const welcome = document.getElementById('welcome').querySelector('h3')
+
+    // was curious. that lead me to figuring out how i would set a interval.
+    // setTimeout(() => {
+    //     if (isMorning) {
+    //         delay.textContent = 'WAKE UP!'
+    //     } else if (isAfternoon) {
+    //         delay.textContent = 'TIME TO WORK!'
+    //     } else if (isEvening) {
+    //         delay.textContent = 'TIME TO RELAX!'
+    //     }
+    // }, 5000)
+    if (welcome) {welcome.textContent = messages}
+    let inx = 0
+    welcome.textContent = messages[inx]
+    setInterval(() => {
+        inx = (inx + 1) % messages.length
+        welcome.textContent = messages[inx]
+    }, 3000)
+    
+})
